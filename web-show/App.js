1244b2dad013435d7c4a65d361ffc9ebd189ad8a
Sachin Yadav <54879903+yadavsachin23@users.noreply.github.com>
2021-03-01 07:35:21 +0530

    Add files via upload

--- web-show/App.js
+++ web-show/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import Card from "./Cards";
+import sdata from "./Sdata";
+import "./index.css";
 
+/*
 function App(params) {
 
   let curDate = new Date();
@@ -27,6 +31,54 @@ function App(params) {
       <h1>Hello,<span className="span" style={cssStyle}> {greeting} </span> </h1>
     </div>
   </>)
-}
+  */
+
+  
+  console.log(sdata[0]);
+  
+  
+    // WE CAN PASS THIS FUNCTION IN {sdata.map(HERE)}
+  function ncard(val){
+    console.log(val);
+    return(
+      <Card
+      key={val.id}
+      imgsrc={val.imglink}
+      title={val.title}
+      sname={val.Name}
+      link={val.link}
+    />
+    )   
+  };
+  
+  // let a = 14;
+  // let b = 14;
+  
+  // let sum = a + b;
+  // console.log(sum)
+  
+  let App = () => (
+    <>
+      <h1 className="heading_style">Best Web Series In India</h1>
+  
+      {sdata.map(ncard)}
+  
+  
+    {/* WE CAN GET VALUE BY USING THIS ARROW FUNCTION */}
+      {/* {sdata.map((val) => {
+        return(
+      <Card
+      imgsrc={val.imglink}
+      title={val.title}
+      sname={val.Name}
+      link={val.link}
+    />
+    );
+      })
+      } */}
+  
+    </>
+  );
+   
 
 export default App;
