dc90b96b133e2b0052c81c8018ad64b2c4e00632
Sachin Yadav <54879903+yadavsachin23@users.noreply.github.com>
2021-03-03 08:05:51 +0530

    Add files via upload

--- Digital-Clock/App.js
+++ Digital-Clock/App.js
@@ -1,55 +1,32 @@
-import React, { useState } from "react";
-
-// const IncNum = () => {
-//    const state = useState();
-
-//    const [count] = useState(0);
-
-// console.log("Clicked " + count++);
+import React, { useState } from 'react';
 
 const App = () => {
-  /* const state = useState();
-  const [count , setCount] = useState(0);
-  
-  const IncNum = () => {
-   setCount(count + 1);  
-  //  console.log("Clicked " + count++);
- }; */
+  const colorR = '#8e44ad';
+  const [bg , setBg] = useState(colorR);
+  // const click = 'Click Me'
+  const [name , setName] = useState('Click Me');
 
-  // Getting time on button click
 
-  //   let newDate = new Date().toLocaleTimeString();
-  //   const [cTime, setcTime] = useState(newDate);
+  const bgchange = () =>{
+    // console.log("Clicked");
+    let newBg = "#34495e";
+    setBg(newBg);
+    // let newName = "Go to Hell 😄 "
+    setName("Go to Hell 😄 ");
+  }
 
-  //  const updateTime = () => {
-  //    let newCTime = new Date().toLocaleTimeString();
-  //    setcTime(newCTime);
-  //  }
+  const bgName = () => {
+    setBg(colorR);
+    setName("Welcome Back 🤗")
+  }
 
-  //   return (
-  //     <>
-  //       {/* <h1> {count} </h1>
-  //       <button onClick = {IncNum}>Click Me</button> */}
-  // <h1>TIME is {cTime}</h1>
-  // <button onClick = { updateTime }>Get Time</button>
-
-  //  Gettting time digitally
-
-  let time = new Date().toLocaleTimeString();
-  const [cTime, setTime] = useState(time);
-
-  const updateTime = () => {
-    time = new Date().toLocaleTimeString();
-    setTime(time);
-  };
-
-  setInterval(updateTime, 1000);
-
-  return (
+  return(
     <>
-      <h1> {cTime} </h1>
+      <div style = {{backgroundColor : bg}}>
+        <button onClick = { bgchange } onDoubleClick = { bgName }> { name } </button>
+      </div>
     </>
-  );
-};
+  )
+}
 
 export default App;
